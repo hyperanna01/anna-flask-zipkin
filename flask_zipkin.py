4c3f3f521eef3380de1e6129d9a0e0ee7d6891d1
brian-hyperanna <brian@hyperanna.com>
2019-05-06 13:42:43 +1000

    version number is cheap

--- flask_zipkin.py
+++ flask_zipkin.py
@@ -13,7 +13,7 @@ from py_zipkin import Encoding
 
 
 
-__version_info__ = ('0', '0', '5')
+__version_info__ = ('0', '0', '6')
 __version__ = '.'.join(__version_info__)
 __author__ = 'Hyper Anna'
 __license__ = 'BSD'
